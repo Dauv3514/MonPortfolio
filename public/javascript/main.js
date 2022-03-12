07708912f9c86aa826aa5813605733ac2e356543
Valentin Dauvier <valentindauvier@hotmail.fr>
2022-03-12 22:22:38 +0100

    responsive complet avec mon image de dos

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -55,3 +55,15 @@ function close() {
 
 }
 
+// Hover sur mobile
+
+let overlay = document.getElementsByClassName('bouton');
+
+overlay.addEventListener("touchstart", function() {
+  overlay.classList.add("hovermobile");
+})
+
+overlay.addEventListener("touchend", function() {
+  overlay.classList.remove("hovermobile");
+})
+
